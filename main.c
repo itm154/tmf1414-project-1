6a4cf582f47d767d3805f09abed926a12bec2e4d
itm154 <ashrulfahmi@gmail.com>
2024-12-09 14:38:29 +0800

    Template

--- main.c
+++ main.c
@@ -1,6 +1,42 @@
+// Requirements
+// - Display menu for user to take orders
+// - Allow maximum of 4 packs of MK per order
+// - Display details of MK orders made by each customer
+// - Display the calculated total price in a single receipt
+// - Take order continuously from customers until user choose to exit
+// - Display the transaction details at anytime when it is needed
+// - Save all the order transaction to a file
+
 #include <stdio.h>
 
-int main() {
-  printf("Hello world");
-  return 0;
-}
+/* ******************************** */
+/* *        Constant values       * */
+/* ******************************** */
+const float MK_KOSONG = 4.50;
+const float MK_AYAM = 7.00;
+const float MK_DAGING = 8.00;
+const float MK_TENDON = 13.00;
+
+const float MK_AYAM_SP = 9.00;
+const float MK_DAGING_SP = 10.00;
+const float MK_TENDON_SP = 16.00;
+
+const float EX_MEE = 1.50;
+const float EX_CHICKEN = 2.00;
+const float EX_MEAT = 2.50;
+const float EX_TENDON = 3.00;
+
+/* ********************************** */
+/* *      Function Prototypes       * */
+/* ********************************** */
+void displayMenu();
+
+/* **************************** */
+/* *      Main Function       * */
+/* **************************** */
+int main() { return 0; }
+
+/* *********************************** */
+/* *      Function Definitions       * */
+/* *********************************** */
+void displayMenu() {}
